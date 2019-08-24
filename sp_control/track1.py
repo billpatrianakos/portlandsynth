e23ec84b3a327e9198a0e19441c5b484815e0a59
Bill Patrianakos <bill@billpatrianakos.me>
2019-08-24 15:23:32 -0500

    syntax error fixs.

--- sp_control/track1.py
+++ sp_control/track1.py
@@ -17,7 +17,7 @@ pot_4_prev = None
 pot_5_prev = None
 
 while True:
-    if pot_1 != pot_1_prev || pot_2 != pot_2_prev || pot_3 != pot_3_prev || pot_4 != pot_4_prev || pot_5 != pot_5_prev
+    if pot_1 != pot_1_prev or pot_2 != pot_2_prev or pot_3 != pot_3_prev or pot_4 != pot_4_prev or pot_5 != pot_5_prev:
         sender.send_message('/trigger/prophet', [pot_1, pot_2, pot_3])
         pot_1_prev = pot_1
         pot_2_prev = pot_2
