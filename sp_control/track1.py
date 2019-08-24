b7cae2c16a43c959e03fbae321313dbc2a7c7201
Bill Patrianakos <bill@billpatrianakos.me>
2019-08-24 18:19:07 -0500

    Refactor test and first control script. Lots of debugging happening.

--- sp_control/track1.py
+++ sp_control/track1.py
@@ -14,23 +14,17 @@ def proper_round(num, dec=0):
 pot_1 = MCP3008(channel=0)
 pot_2 = MCP3008(channel=1)
 pot_3 = MCP3008(channel=2)
-pot_4 = MCP3008(channel=3)
-pot_5 = MCP3008(channel=4)
 
-pot_1_prev = None
-pot_2_prev = None
-pot_3_prev = None
-pot_4_prev = None
-pot_5_prev = None
+volume_prev  = None
+cutoff_prev  = None
+sustain_prev = None
 
 while True:
-    if pot_1.value != pot_1_prev or pot_2.value != pot_2_prev or pot_3.value != pot_3_prev or pot_4.value != pot_4_prev or pot_5.value != pot_5_prev:
-        volume = proper_round(pot_1.value)
-        cutoff = proper_round(pot_2.value * 10)
-        sustain = proper_round(pot_3.value + 0.2, 1)
+    volume = proper_round(pot_1.value)
+    cutoff = proper_round(pot_2.value * 10)
+    sustain = proper_round(pot_3.value + 0.2, 1)
+    if volume != volume_prev or cutoff != cutoff_prev or sustain != sustain_prev:
         sender.send_message('/trigger/prophet', [volume, cutoff, sustain])
-        pot_1_prev = pot_1.value
-        pot_2_prev = pot_2.value
-        pot_3_prev = pot_3.value
-        pot_4_prev = pot_4.value
-        pot_5_prev = pot_5.value
+        volume_prev  = volume
+        cutoff_prev  = cutoff
+        sustain_prev = sustain
