df406fa04743581c205aead2c6ff17641af23ed7
Bill Patrianakos <bill@billpatrianakos.me>
2019-08-25 13:02:16 -0500

    Slight improvements to Python generated audio.

--- pysounds/sine.py
+++ pysounds/sine.py
@@ -31,7 +31,8 @@ while True:
     if volume != volume_prev or duration != duration_prev or f != f_prev:
         p = pyaudio.PyAudio()
         # generate samples, note conversion to float32 array
-        samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+        # samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+        samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32).tobytes()
 
         # for paFloat32 sample values must be in range [-1.0, 1.0]
         stream = p.open(format=pyaudio.paFloat32,
@@ -40,7 +41,8 @@ while True:
                         output=True)
 
         # play. May repeat with different volume values (if done interactively) 
-        stream.write(volume*samples)
+        # stream.write(volume*samples)
+        stream.write(samples)
 
         stream.stop_stream()
         stream.close()
