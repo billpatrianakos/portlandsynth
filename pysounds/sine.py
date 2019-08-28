d90bb016d8a9615d41d62c55eb30b00f37a3302c
Bill Patrianakos <bill@billpatrianakos.me>
2019-08-28 16:11:05 -0500

    Make the sine script a little more efficient. Maybe just a little.

--- pysounds/sine.py
+++ pysounds/sine.py
@@ -15,43 +15,34 @@ pot_2 = MCP3008(channel=1)
 pot_3 = MCP3008(channel=2)
 pot_4 = MCP3008(channel=4)
 
-volume_prev     = None
-duration_prev   = None
-f_prev          = None
-
-# p = pyaudio.PyAudio()
+p = pyaudio.PyAudio()
 
 fs = 48000 # sampling rate, Hz, must be integer
 
 while True:
     sine_freq_multiplier    = pot_4.value
-    volume   = proper_round(pot_1.value)          # range [0.0, 1.0]
-    duration = proper_round(pot_2.value * 10)     # in seconds, may be float
-    f        = proper_round(pot_3.value * (sine_freq_multiplier * 10), 1) # sine frequency, Hz, may be float
+    volume                  = proper_round(pot_1.value)          # range [0.0, 1.0]
+    duration                = proper_round(pot_2.value * 10)     # in seconds, may be float
+    f                       = proper_round(pot_3.value * (sine_freq_multiplier * 10), 1) # sine frequency, Hz, may be float
+    
     print(volume, duration, f, sine_freq_multiplier)
-    if volume != volume_prev or duration != duration_prev or f != f_prev:
-        p = pyaudio.PyAudio()
-        # generate samples, note conversion to float32 array
-        # samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
-        samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32).tobytes()
+    
+    # p = pyaudio.PyAudio()
+    # generate samples, note conversion to float32 array
+    # samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+    samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32).tobytes()
 
-        # for paFloat32 sample values must be in range [-1.0, 1.0]
-        stream = p.open(format=pyaudio.paFloat32,
-                        channels=1,
-                        rate=fs,
-                        output=True)
+    # for paFloat32 sample values must be in range [-1.0, 1.0]
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=fs,
+                    output=True)
 
-        # play. May repeat with different volume values (if done interactively) 
-        # stream.write(volume*samples)
-        stream.write(samples)
+    # play. May repeat with different volume values (if done interactively) 
+    # stream.write(volume*samples)
+    stream.write(samples)
 
-        stream.stop_stream()
-        stream.close()
+    stream.stop_stream()
+    stream.close()
 
-        p.terminate()
-
-        volume_prev     = volume
-        duration_prev   = duration
-        f_prev          = f
-        sine_freq_multiplier_prev      = sine_freq_multiplier
-        # sleep(sine_freq_multiplier)
+    p.terminate()
