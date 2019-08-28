45a21acd06a05d67c6612d3f4797a4d6325e758a
Bill Patrianakos <bill@billpatrianakos.me>
2019-08-28 16:09:43 -0500

    Remove sonic pi files. No longer going in that direction.
    OSC cues only work with Sonic Pi 3+ and support for it is spotty on Raspbian
    Buster.

--- sonic_pi/track1.rb
+++
@@ -1,7 +0,0 @@
-@osc_server ||=  SonicPi::OSC::UDPServer.new(4559, use_decoder_cache: true) #__nosave__
-
-live_loop :foo do
-  use_real_time
-  a, b, c = sync "/osc/trigger/prophet"
-  synth :prophet, note: a, cutoff: b, sustain: c
-end
